9a430a8b0afb1fe1001792ad690f3cf0f6810e47
asinsayedali <asinsayedali008@gmail.com>
2024-06-28 00:02:08 +0530

    added two new exercises

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -4,14 +4,14 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    sum=0
-    if number<1:
+    sum = 0
+    if number < 1:
         raise ValueError("Classification is only possible for positive integers.")
-    for i in range(1,number):
-        if number%i==0:
+    for i in range(1, number):
+        if number % i == 0:
             sum = sum +i
 
-    if sum ==number:
+    if sum == number:
         result="perfect"
         return result
     elif sum>number:
--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,14 +1,37 @@
+import re
+
+
 class PhoneNumber:
-    def __init__(self, number):
-        self.number = number
-        if len(self.number) < 10:
+    def __init__(self, phone_number):
+        if re.search(r"[a-zA-Z]", phone_number):
+            raise ValueError("letters not permitted")
+        if re.search(r"[^0-9-().+ ]", phone_number):
+            raise ValueError("punctuations not permitted")
+        clean = "".join([x for x in phone_number if x.isdigit()])
+        if len(clean) < 10:
             raise ValueError("must not be fewer than 10 digits")
-        elif len(self.number) > 11 and self.number[0] != 1:
+        if len(clean) == 11 and clean[0] != "1":
             raise ValueError("11 digits must start with 1")
-        elif len(self.number) > 11:
+        if len(clean) > 11:
             raise ValueError("must not be greater than 11 digits")
-        else:
-            print("not value ")
-        
+        self.clean = clean[1:] if len(clean) == 11 else clean
+        if self.clean[0] == "0":
+            raise ValueError("area code cannot start with zero")
+        if self.clean[0] == "1":
+            raise ValueError("area code cannot start with one")
+        if self.clean[3] == "0":
+            raise ValueError("exchange code cannot start with zero")
+        if self.clean[3] == "1":
+            raise ValueError("exchange code cannot start with one")
 
+    @property
+    def number(self):
+        return self.clean
 
+    @property
+    def area_code(self):
+        return self.number[:3]
+
+    def pretty(self):
+        num = self.number
+        return "(" + num[:3] + ")-" + num[3:6] + "-" + num[6:]
