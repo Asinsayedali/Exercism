8f457877d2d48594c0b77403717ea08c0d741133
asinsayedali <asinsayedali008@gmail.com>
2025-01-09 07:20:35 +0530

    added exercise change

--- change/change.py
+++ change/change.py
@@ -1,2 +1,17 @@
-def find_fewest_coins(coins, target):
-    pass
+from itertools import combinations_with_replacement
+from typing import Iterator
+
+
+def find_fewest_coins(coins: list[int], target: int) -> list[int]:
+    if target < 0:
+        raise ValueError("target can't be negative")
+
+    max_coins: int = 1 + target // min(coins)
+
+    for i in range(max_coins):
+        combinations: Iterator[tuple[int, ...]] = combinations_with_replacement(coins, i)
+        for combination in combinations:
+            if sum(combination) == target:
+                return sorted(combination)
+
+    raise ValueError("can't make target with given coins")
