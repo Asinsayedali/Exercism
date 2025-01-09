352b36274b900b0313bd6a87a328fdd68a1335b5
asinsayedali <asinsayedali008@gmail.com>
2025-01-10 00:37:19 +0530

    Made changes to code

--- change/change.py
+++ change/change.py
@@ -5,11 +5,13 @@ from typing import Iterator
 def find_fewest_coins(coins: list[int], target: int) -> list[int]:
     if target < 0:
         raise ValueError("target can't be negative")
+    if not target:
+        return []
 
-    max_coins: int = 1 + target // min(coins)
-
-    for i in range(max_coins):
-        combinations: Iterator[tuple[int, ...]] = combinations_with_replacement(coins, i)
+    max_coins = 1 + target // min(coins)
+    coins = sorted(coins)
+    for i in range(1, max_coins):
+        combinations = combinations_with_replacement(coins, i)
         for combination in combinations:
             if sum(combination) == target:
                 return sorted(combination)
